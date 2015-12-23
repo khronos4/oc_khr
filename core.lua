8b5fbca85863aacf18f3fa80c6733241296f4d41
Kirill Kropivyanskiy <666khronos@gmail.com>
2015-12-23 17:48:18 +0100

    syntax fixes

--- core.lua
+++ core.lua
@@ -45,7 +45,7 @@ function module.log_info(...)
 end
 
 -- logging
-function module.log_error(text, ...)
+function module.log_error(...)
   local str = ""
   for i,v in ipairs(arg) do
     str = str .. tostring(v) .. " "
